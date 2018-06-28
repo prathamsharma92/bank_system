c9bdc757e6f33e9a4032c32570ee33e3660e5d3d
Pratham sharma <pratham.sharma@mobiloittegroup.com>
2018-06-28 20:50:22 +0530

    create devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :managers
-  devise_for :users
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      registrations: 'users/registrations'
+    }
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
